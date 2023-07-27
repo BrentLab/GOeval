058cf9554fe5278023d39ae0be70ed26e7839d9e
westbrooktm <westbrookt@wustl.edu>
2023-07-27 13:34:24 -0400

    minor formatting changes

--- R/evaluate.R
+++ R/evaluate.R
@@ -3,31 +3,41 @@
 #' @description
 #' Given one network, `evaluate` will first make subsets with `subset_network`,
 #'  then run Over-Representation Analysis (ORA) with `webgestalt_network`,
-#'  followed by `get_metrics` and `plot_metrics` to plot selected summary statistics.
+#'  followed by `get_metrics` and `plot_metrics` to plot selected summary
+#'  statistics.
 #'
-#' @param network path to the file containing the network to create subsets from. The file should
-#'  be tab-separated with three columns: source node, target node, edge score
-#' @param reference_set path to the set of all genes possibly included in the network. Must be a .txt
-#'  file containing exactly one column of the genes that could possibly appear in the network.
-#' @param output_directory path to the directory in which to store the generated network subsets,
-#'  ORA summaries, and plots
-#' @param network_name short name for the network - used in file naming so may not contain spaces
+#' @param network path to the file containing the network to create subsets
+#'  from. The file should be tab-separated with three columns: source node,
+#'  target node, edge score
+#' @param reference_set path to the set of all genes possibly included in the
+#'  network. Must be a .txt file containing exactly one column of the genes that
+#'  could possibly appear in the network.
+#' @param output_directory path to the directory in which to store the generated
+#'  network subsets, ORA summaries, and plots
+#' @param network_name short name for the network - used in file naming so may
+#'  not contain spaces
 #' @param organism a string specifying the organism that the data is from, e.g.
 #'    "hsapiens" or "scerevisiae" - see options with WebGestaltR::listOrganism()
-#' @param database the gene set database to search for enrichment - see options with WebGestaltR::listGeneSet().
-#'  Must be a Gene Ontology "biological process" database if get_annotation_overlap = TRUE.
-#' @param gene_id the naming system used for the input genes - see options with WebGestaltR::listIdType()
-#'  and see webgestalt.org for examples of each type
-#' @param edges list of total numbers of edges or average edges per TF to include in each subset
-#' @param num_possible_TFs if set to a number > 0, the elements of 'edges' will first be
-#'  multiplied by this number to get the number of edges for each subset
-#' @param permutations the number of randomly permuted networks to create and run ORA on
-#' @param penalty the penalty applied to the 'sum' metric for each TF in the network
-#' @param fdr_threshold the FDR threshold for a gene set term to be considered significantly
-#'  over-represented for the purposes of calculating the 'percent' metric
-#' @param get_sum boolean whether to get and plot the 'sum' metric, which is the sum of the negative
-#'  log base 10 of the p-value for the top term of each source node minus 'penalty' times the total
-#'  number of source nodes.
+#' @param database the gene set database to search for enrichment - see options
+#'  with WebGestaltR::listGeneSet(). Must be a Gene Ontology
+#'  "biological process" database if get_annotation_overlap = TRUE.
+#' @param gene_id the naming system used for the input genes - see options with
+#'  WebGestaltR::listIdType() and see webgestalt.org for examples of each type
+#' @param edges list of total numbers of edges or average edges per TF to
+#'  include in each subset
+#' @param num_possible_TFs if set to a number > 0, the elements of 'edges' will
+#'  first be multiplied by this number to get the number of edges for each
+#'  subset
+#' @param permutations the number of randomly permuted networks to create and
+#'  run ORA on
+#' @param penalty the penalty applied to the 'sum' metric for each TF in the
+#'  network
+#' @param fdr_threshold the FDR threshold for a gene set term to be considered
+#'  significantly over-represented for the purposes of calculating the 'percent'
+#'  metric
+#' @param get_sum boolean whether to get and plot the 'sum' metric, which is the
+#'  sum of the negative log base 10 of the p-value for the top term of each
+#'  source node minus 'penalty' times the total number of source nodes.
 #' @param get_percent boolean whether to get and plot the 'percent' metric, which is the
 #'  percent of source nodes with at least one term with a FDR below the 'fdr_threshold'
 #' @param get_mean boolean whether to get and plot the 'mean' metric, which is the mean negative
